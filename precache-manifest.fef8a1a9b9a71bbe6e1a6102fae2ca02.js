615df96d8e55bf1e20f003ec5d2f71e3419010b0
First Last <salmanashraf853@gmail.com>
2020-02-06 00:27:22 +0100

    Updates

---
+++ precache-manifest.fef8a1a9b9a71bbe6e1a6102fae2ca02.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac47eac6044d6fe963c28772d8e6b310",
+    "url": "/bincom_test/index.html"
+  },
+  {
+    "revision": "06faa6b851b9a84a8f42",
+    "url": "/bincom_test/static/css/2.a85399c1.chunk.css"
+  },
+  {
+    "revision": "b1c407bcf105beca52ba",
+    "url": "/bincom_test/static/css/main.a3534be7.chunk.css"
+  },
+  {
+    "revision": "06faa6b851b9a84a8f42",
+    "url": "/bincom_test/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "b1c407bcf105beca52ba",
+    "url": "/bincom_test/static/js/main.1d97dc9d.chunk.js"
+  },
+  {
+    "revision": "f4985d11d5e32659ddab",
+    "url": "/bincom_test/static/js/runtime~main.589c2e2f.js"
+  },
+  {
+    "revision": "cffa9d9d88890cfcd0ae3d6dd7a6efa9",
+    "url": "/bincom_test/static/media/3.cffa9d9d.gif"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/bincom_test/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/bincom_test/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/bincom_test/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/bincom_test/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/bincom_test/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
